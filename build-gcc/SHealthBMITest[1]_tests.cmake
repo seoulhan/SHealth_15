da2d5a33c0b1be9cb11399ee4b37575d2e7a7ee1
seoulhan <seoulhanknox@gmail.com>
2026-05-20 09:10:03 +0900

    04_리팩토링_Refactoring_Support

---
+++ build-gcc/SHealthBMITest[1]_tests.cmake
@@ -0,0 +1,1 @@
+set(  SHealthBMITest_TESTS)
---
+++ build-mingw/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/DEV/SHealth_15
+# Build directory: C:/DEV/SHealth_15/build-mingw
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("C:/DEV/SHealth_15/build-mingw/SHealthBMITest[1]_include.cmake")
+subdirs("_deps/googletest-build")
